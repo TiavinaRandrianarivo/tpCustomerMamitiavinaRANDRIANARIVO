f436e121dd4858879087f7a4b61ac713778029c2
TiavinaRandrianarivo <tiavinarandy24@gmail.com>
2023-03-20 20:34:09 +0300

    Ajout EJB pour les codes de réduction

---
+++ src/main/java/mg/itu/tpcustomermamitiavinarandrianarivo/ejb/DiscountManager.java
@@ -0,0 +1,28 @@
+package mg.itu.tpcustomermamitiavinarandrianarivo.ejb;  // A MODIFIER...
+          
+import mg.itu.tpcustomermamitiavinarandrianarivo.entities.Discount;
+import java.util.List;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+@Stateless
+public class DiscountManager {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Discount> getAllDiscounts() {
+        Query query = em.createNamedQuery("Discount.findAll");
+        return query.getResultList();
+    }
+
+    public Discount findById(String code) {
+        return em.find(Discount.class, code);
+    }
+
+    public void persist(Discount discount) {
+        em.persist(discount);
+    }
+}
